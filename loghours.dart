adb7723e97d277e94b03c7cf77e55438ff1afd93
Alex Lindroos <alex.lindroos@codemate.com>
2018-10-31 15:20:06 +0200

    Fixed the logger to continously ask the right arguments if not provided.

--- loghours.dart
+++ loghours.dart
@@ -1,30 +1,22 @@
 import 'dart:io';
 import 'dart:core';
 
+String startInput, endInput;
+bool loggingStarted = false;
+DateTime startTime, endTime;
+
 void main() {
-  stdout.writeln('Type code to start logging');
-  String startInput = stdin.readLineSync();
-  bool loggingStarted = false;
-  DateTime startTime, endTime;
+
+  while (startInput != "code") {
+    stdout.writeln("Type 'code' to start logging.");
+    startInput = stdin.readLineSync();
+  }
 
   if (startInput == "code") {
       loggingStarted = true;
       startTime = new DateTime.now();
       stdout.writeln("Logging started! Type 'mate' when you stop");
-    } else {
-      stdout.writeln("Type 'code' to start logging.");
-    }
-
-  String endInput = stdin.readLineSync();
-
-  if (endInput == "mate" && loggingStarted) {
-      loggingStarted = false;
-      endTime = new DateTime.now();
-      var usedTime = getUsedTime(startTime, endTime);
-      String usedTimeTrimmed = usedTime.substring(0, 7);
-      stdout.writeln("Logging has end. You used $usedTimeTrimmed Thank you for logging the time!");
-    } else {
-      stdout.writeln("Type 'mate' to end logging.");
+      endLogging();
     }
 }
 
@@ -32,4 +24,20 @@ String getUsedTime(DateTime startTime, DateTime endTime) {
   Duration duration = endTime.difference(startTime);
   String usedTimeString = duration.toString();
   return usedTimeString;
+}
+
+void endLogging() {
+
+  while (endInput != "mate") {
+    stdout.writeln("Type 'mate' to end logging.");
+    endInput = stdin.readLineSync();
+  }
+
+  if (endInput == "mate" && loggingStarted) {
+    loggingStarted = false;
+    endTime = new DateTime.now();
+    var usedTime = getUsedTime(startTime, endTime);
+    String usedTimeTrimmed = usedTime.substring(0, 7);
+    stdout.writeln("Logging has end. You used $usedTimeTrimmed Thank you for logging the time!");
+  }
 }
